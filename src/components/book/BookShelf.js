52decdb83b20687c69179ee9b94a10ead624bbd4
Jordan Oliveira <oliveira.jordan@gmail.com>
2018-12-02 11:09:39 -0200

    feat: Criação de components e suas pastas. Os componentes da página inicial foram organizados na pasta components/book.

---
+++ src/components/book/BookShelf.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import BooksGrid from './BooksGrid';
+
+class BookShelf extends Component {
+
+    render() {
+        const { title, books } = this.props;
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title"> {title}</h2>
+                <div className="bookshelf-books">
+                    <BooksGrid books={books}/>
+                </div>
+            </div>
+        );
+    }
+
+}
+export default BookShelf;
---
+++ src/components/book/BooksGrid.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class BooksGrid extends Component {
+
+    render() {
+        const { books } = this.props;
+        return (
+            <div>Oi</div>
+        );
+    }
+
+}
+
+export default BooksGrid;
